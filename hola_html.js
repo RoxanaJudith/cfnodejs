63c2c1a0ec98ebaf6d373089b54e6ce8867ce5e2
RoxanaJudith <roxanajpr.4@gmail.com>
2020-08-24 10:28:00 -0400

    Encabezado y objeto response

--- hola_html.js
+++ hola_html.js
@@ -3,7 +3,8 @@ var http = require("http"),
 
 http.createServer(function(req, res){
     fs.readFile("./index.html", function(err, html){
-        res.write(html);
+        res.writeHead(200,{"Content-Type":"application/json"}); //text/html en caso de enviar html, application/json en caso de enviar json
+        res.write(JSON.stringify({nombre: "Uriel", username:"roxana"}));
         res.end();
     }); 
 }).listen(8080);    
