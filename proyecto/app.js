1d932065495daa46bb39955c629ce7df38841b65
RoxanaJudith <roxanajpr.4@gmail.com>
2020-09-07 17:42:02 -0400

    Servir archivos estáticos

--- proyecto/app.js
+++ proyecto/app.js
@@ -2,6 +2,10 @@ var express = require("express");
 
 var app = express();
 
+//midominio.com/estatico/
+app.use("/estatico", express.static('public'));  //funcion static retorna midleware necesario que se monta en use que permite servir archivos staticos
+app.use(express.static('assets'));
+
 app.set("view engine","pug");
 
 app.get("/",function(req, res){
