3c2f5cef0f1a3b5405330c9fb64441305c309099
RoxanaJudith <roxanajpr.4@gmail.com>
2020-09-08 16:37:41 -0400

    Leer parámetros

--- proyecto/app.js
+++ proyecto/app.js
@@ -1,19 +1,26 @@
 var express = require("express");
-
+var bodyParser = require("body-parser");
 var app = express();
 
 //midominio.com/estatico/
 app.use("/estatico", express.static('public'));  //funcion static retorna midleware necesario que se monta en use que permite servir archivos staticos
-app.use(express.static('assets'));
+app.use(bodyParser.json()); //para peticiones formato application/json
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine","pug");
 
 app.get("/",function(req, res){
     res.render("index");
-})
+});
 
 app.get("/login",function(req, res){
     res.render("login");
-})
+});
+
+app.post("/users", function(req, res){
+    console.log("Contraseña: " + req.body.password);
+    console.log("Email: " + req.body.email);
+    res.send("Datos recibidos");
+});
 
 app.listen(8080);
